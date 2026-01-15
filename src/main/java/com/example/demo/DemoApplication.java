4a84360a1f2dd16da4c132b03e6cae9d954a467d
AinananShi <ainaryk66@gmail.com>
2026-01-15 10:15:59 +0100

    correction syntaxe DemoApplication2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotaion.GetMapping;
-import org.spirngframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -21,3 +21,5 @@ public class DemoApplication {
 
 	}
 }
+
+
