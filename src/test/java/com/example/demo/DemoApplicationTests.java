46e8098a5edd23cef7545dda8036ca5ac95cb5f1
Aina <152268895+AinananShi@users.noreply.github.com>
2026-01-15 10:31:50 +0100

    Refactor DemoApplicationTests and remove unused code

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,22 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.GetMapping;
-import org.springframework.boot.test.context.RestController;
 
+@SpringBootTest
+class DemoApplicationTests {
 
-@SpringBootApplication
-public class DemoGithubApplication {
-
-	public static main(String[] args){
-		SpringApplication.run(DemoGithubApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
-}
 
-@RestController
-class HelloWorldController{
-	@GetMapping("/")
-	public String sayHello(){
-		return "Nuggets";
-	}
 }
