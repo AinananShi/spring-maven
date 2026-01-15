3b5eb071108a92e5190bba2cab38b8603a40fc03
AinananShi <ainaryk66@gmail.com>
2026-01-15 10:25:19 +0100

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootTest
+class DemoApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@Test
+	void contextLoads() {
 	}
+
 }
 
 @RestController
